982dcf7b53d9ce163a59993b04d732e51573e8fd
sinisti <spideralan2008@gmail.com>
2024-10-29 15:16:34 +0100

    THERE ARE PARASITES IN YOUR SKIN

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
+ 
 session_name('sesje');
 session_start();
-$_SESSION['user'] = 'THE TOWN INSIDE ME';
-echo $_SESSION['user'];
+ 
+$_SESSION["huh"] = "THERE ARE PARASITES IN YOUR SKIN ";
+ 
+$low = 0;
+$high = 255;
+ 
+for($i=0; $i<100000; $i++){
+echo '<p style="color:rgb('.rand($low, $high).','.rand($low, $high).','.rand($low, $high).');background-color:rgb('.rand($low, $high).','.rand($low, $high).','.rand($low, $high).');display:inline">'.$_SESSION["huh"].'</p>' ;
+}
--- sesja.php
+++ sesja.php
@@ -1,4 +1,5 @@
 <?php
 session_name('sesje');
 session_start();
+session_destroy();
 echo $_SESSION['user'];
